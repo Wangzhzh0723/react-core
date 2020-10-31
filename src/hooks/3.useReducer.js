4015a24cfb2f0cc7dd67e37597ebca0000f78fd0
Wangzhzh0723 <1274972209@qq.com>
2020-11-01 02:53:09 +0800

    :sparkles

---
+++ src/hooks/3.useReducer.js
@@ -0,0 +1,86 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+// 所有状态
+const hookStates = []
+// 状态下标
+let hookIndex = 0
+
+function useReducer(reducer, initialState, init) {
+  // 获取第一次定义的的state
+  hookStates[hookIndex] =
+    hookStates[hookIndex] || (init ? init(initialState) : initialState)
+  // 记录state存储的下标
+  const currentIndex = hookIndex
+  function dispatch(action) {
+    const oldState = hookStates[currentIndex]
+    hookStates[currentIndex] = reducer ? reducer(oldState, action) : action
+    render()
+  }
+  // 返回数据  数组好处,不显示定义数据的名字
+  return [hookStates[hookIndex++], dispatch]
+}
+
+// useState是一个简化版的useReducer
+
+function useState(initialState) {
+  return useReducer(null, initialState)
+}
+
+/**
+ * 处理器
+ * 接受一个老状态和一个动作, 返回一个新状态
+ */
+function reducer(state, action) {
+  switch (action.type) {
+    case "add":
+      state.number += 1
+      break
+    case "minus":
+      state.number -= 1
+      break
+    default:
+      break
+  }
+  return { ...state }
+}
+
+let initialState = 0
+
+function init(initialState) {
+  return { number: initialState }
+}
+
+function Counter() {
+  const [state, dispatch] = useReducer(reducer, initialState, init)
+
+  return (
+    <div>
+      <p>number: {state.number}</p>
+      <button
+        onClick={() => {
+          dispatch({
+            type: "add"
+          })
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          dispatch({
+            type: "minus"
+          })
+        }}
+      >
+        -
+      </button>
+    </div>
+  )
+}
+function render() {
+  hookIndex = 0
+  ReactDOM.render(<Counter />, document.getElementById("root"))
+}
+
+render()
